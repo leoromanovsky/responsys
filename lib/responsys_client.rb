50b0edcdd35242de25df831b8fb967534766a19c
Leo Romanovsky <leo@strava.com>
2012-11-07 14:58:27 -0800

    added Rakefile and empty test

--- lib/responsys_client.rb
+++ lib/responsys_client.rb
@@ -1,3 +1,16 @@
+require 'metal/defaultDriver.rb'
+require 'metal/defaultMappingRegistry.rb'
+
 class ResponsysClient
+
+  # Creates a client object to connect to Responsys via SOAP API
+  #
+  # <username> - The login username
+  # <password> - The login password
+  def initialize(username, password)
+    @username = username
+    @password = password
+    @client = ResponsysWS.new
+  end
 
 end
---
+++ test/test_responsys_client.rb
@@ -0,0 +1,6 @@
+require 'test/unit'
+require 'responsys_client'
+
+class ResponsysClientTest < Test::Unit::TestCase
+
+end
