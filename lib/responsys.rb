4cb4bb044780f87878d6e8ef8a4a5d7c68d13688
Leo Romanovsky <leo@strava.com>
2012-11-07 16:58:02 -0800

    added support for login, logout and email triggering

--- lib/responsys.rb
+++ lib/responsys.rb
@@ -2,6 +2,17 @@ require 'metal/defaultDriver.rb'
 require 'metal/defaultMappingRegistry.rb'
 
 class Responsys
+  TIMEOUT = 10
+  MAX_MEMBERS = 200
+
+  class ResponsysTimeoutError < StandardError
+  end
+
+  class MethodsNotSupportedError < StandardError
+  end
+
+  attr_reader :session_id
+
   # Creates a client object to connect to Responsys via SOAP API
   #
   # <username> - The login username
@@ -12,4 +23,111 @@ class Responsys
     @client = ResponsysWS.new
   end
 
+  def login
+    with_application_error do
+      login_request = Login.new
+      login_request.username = @username
+      login_request.password = @password
+      response = @responsys_client.login login_request
+      @session_id = response.result.sessionId
+      assign_session
+    end
+  end
+
+  def assign_session
+    session_header_request = SessionHeader.new
+    session_header_request.sessionId = @session_id
+    @responsys_client.headerhandler.add session_header_request
+  end
+
+  def logout
+    begin
+      logout_request = Logout.new
+      @responsys_client.logout logout_request
+    ensure
+      @session_id = nil
+    end
+  end
+
+  # Sends a campaign email to the list of recipients
+  #
+  # <campaign_name> - The type of email being sent.
+  # <recipients> - A list of recipients to send the email to.
+
+  #  Each element in the list is a hash:
+  #  {
+  #    id: athlete_id,
+  #    email: email address
+  #  }
+  #
+  def send_email(campaign_name, recipients, recipient_options = nil)
+    trigger_campaign_message = TriggerCampaignMessage.new
+    interact_object = InteractObject.new
+    interact_object.folderName = 'ignored'
+    interact_object.objectName = campaign_name
+    trigger_campaign_message.campaign = interact_object
+    trigger_campaign_message.recipientData = []
+
+    recipients.each_with_index do |recipient_info, i|
+      #options = recipient_options[i]
+
+      # Responsys requires something in the optional data for SOAP bindings to work
+      options = {foo: :bar}
+
+      recipient = Recipient.new
+      recipient.emailAddress = recipient_info[:email] if recipient_info[:email]
+      recipient.customerId = recipient_info[:id] if recipient_info[:id]
+      recipient_data = RecipientData.new
+      recipient_data.recipient = recipient
+      recipient_data.optionalData = []
+      options.each_pair do |k, v|
+        optional_data = OptionalData.new
+        optional_data.name = k
+        v.gsub!(/[[:cntrl:]]/, ' ') if v.is_a? String
+        optional_data.value = v
+        recipient_data.optionalData << optional_data
+      end
+
+      trigger_campaign_message.recipientData << recipient_data
+    end
+
+    with_session do
+      @client.triggerCampaignMessage(trigger_campaign_message)
+    end
+  end
+
+  def with_timeout
+    Timeout::timeout(timeout_threshold, ResponsysTimeoutError) do
+      yield
+    end
+  end
+
+  def with_session
+    begin
+      with_timeout do
+        login if @session_id.nil?
+      end
+      with_application_error do
+        with_timeout do
+          yield
+        end
+      end
+    ensure
+      with_timeout do
+        logout unless @keep_alive
+      end
+    end
+  end
+
+  protected
+
+  def with_application_error
+    begin
+      yield
+    rescue SOAP::FaultError => e
+      inner_e = e.detail[e.faultstring.data]
+      raise inner_e if inner_e
+      raise e
+    end
+  end
 end
